ebacf1b8a36ad17cdea3a85cb9a7cf954e2d5dc7
amol234545 <amolmilton@gmail.com>
2024-03-16 11:23:37 -0400

    more changes

--- help.lua
+++
@@ -1,9 +0,0 @@
-local commands = {
-   
-}
-return function() 
-   print("bloxPackage")
-   print("A package manager for Roblox.")
-   print("Commands:")
-   for _,v in commands do print(v) end
-end
--- logc.lua
+++
@@ -1,7 +0,0 @@
-local stdio = require("@lune/stdio")
-function logc(message : string | any, color: "black" | "blue" | "cyan" | "green" | "purple" | "red"  | "white" | "yellow")
-    stdio.write(stdio.color(color))
-    print(message)
-    stdio.write(stdio.color("reset"))
-end
-return logc
